81d4f5afcc1ae179ffe11f20181e568d0bc0d858
Aalaa Ayman <116173402+AalaaAyman24@users.noreply.github.com>
2024-11-17 14:07:41 +0200

    Update header-scroll.js

--- Project/javascript/header-scroll.js
+++ Project/javascript/header-scroll.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     if (window.scrollY > 50) {
-        header.classList.add('scrolled'); // إضافة كلاس "scrolled" عند التمرير
+        header.classList.add('scrolled'); 
     } else {
-        header.classList.remove('scrolled'); // إزالة الكلاس عند الرجوع لأعلى
+        header.classList.remove('scrolled'); 
     }
 });
